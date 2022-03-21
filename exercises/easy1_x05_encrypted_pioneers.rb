1e1dbbc304e1981190361b7abf6e542ceafdc223
oscos <cortes.oscar@outlook.com>
2022-03-21 06:04:21 +0000

    added additional solutions

--- exercises/easy1_x05_encrypted_pioneers.rb
+++ exercises/easy1_x05_encrypted_pioneers.rb
@@ -1,7 +1,5 @@
 def rot13(str)
-  # letters = ('a'..'z').to_a * 2
-  letters = ('a'..'z').to_a
-  letters += ('a'..'z').to_a 
+  letters = ('a'..'z').to_a * 2
   str.chars.map do |el|
     idx = letters.index(el.downcase)
     if idx
@@ -32,3 +30,55 @@ p rot13('Zneiva Zvafxl')
 p rot13('Lhxvuveb Zngfhzbgb')
 p rot13('Unllvz Fybavzfxv')
 p rot13('Tregehqr Oynapu')
+
+
+# method 2
+AM, NZ = ('a'..'z').each_slice(13).to_a
+
+def convert_char(single_char)
+  result =
+    if AM.include?(single_char.downcase)
+      (single_char.downcase.ord + 13).chr
+    elsif NZ.include?(single_char.downcase)
+      (single_char.downcase.ord - 13).chr
+    else
+      single_char
+    end
+  single_char.downcase == single_char ? result : result.upcase
+end
+
+def decrypt(name)
+  name.chars.map do |single_char|
+    convert_char(single_char)
+  end.join
+end
+
+def rot13_2(arr)
+  arr.map do |names|
+    decrypt(names)
+  end
+end
+
+ENCRYPTED_PIONEERS = [
+  'Nqn Ybirynpr',
+  'Tenpr Ubccre',
+  'Nqryr Tbyqfgvar',
+  'Nyna Ghevat',
+  'Puneyrf Onoontr',
+  'Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv',
+  'Wbua Ngnanfbss',
+  'Ybvf Unvog',
+  'Pynhqr Funaaba',
+  'Fgrir Wbof',
+  'Ovyy Tngrf',
+  'Gvz Orearef-Yrr',
+  'Fgrir Jbmavnx',
+  'Xbaenq Mhfr',
+  'Fve Nagbal Ubner',
+  'Zneiva Zvafxl',
+  'Lhxvuveb Zngfhzbgb',
+  'Unllvz Fybavzfxv',
+  'Tregehqr Oynapu'
+].freeze
+
+p rot13_2(ENCRYPTED_PIONEERS)
