ae2325cb4e5fc585e50534939c2d0ad45909f6fe
oscos <cortes.oscar@outlook.com>
2022-07-03 19:09:02 +0000

    Add additional solutions.

--- exercises/easy1_x05_encrypted_pioneers.rb
+++ exercises/easy1_x05_encrypted_pioneers.rb
@@ -11,25 +11,82 @@ def rot13(str)
   end.join
 end
 
-p rot13('Nqn Ybirynpr')
-p rot13('Tenpr Ubccre')
-p rot13('Nqryr Tbyqfgvar')
-p rot13('Nyna Ghevat')
-p rot13('Puneyrf Onoontr')
-p rot13('Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv')
-p rot13('Wbua Ngnanfbss')
-p rot13('Ybvf Unvog')
-p rot13('Pynhqr Funaaba')
-p rot13('Fgrir Wbof')
-p rot13('Ovyy Tngrf')
-p rot13('Gvz Orearef-Yrr')
-p rot13('Fgrir Jbmavnx')
-p rot13('Xbaenq Mhfr')
-p rot13('Fve Nagbal Ubner')
-p rot13('Zneiva Zvafxl')
-p rot13('Lhxvuveb Zngfhzbgb')
-p rot13('Unllvz Fybavzfxv')
-p rot13('Tregehqr Oynapu')
+# additional solution added 07/03/2022
+def rot13(str)
+  uppercase = ('A'..'Z').to_a * 2
+  lowercase = ('a'..'z').to_a * 2
+  result = ''
+
+  str.chars.each.with_index do |chr, idx|
+    if uppercase.include?(chr)
+      idx13 = uppercase.index(chr) + 13
+      result += (uppercase[idx13])
+    elsif lowercase.include?(chr)
+      idx13 = lowercase.index(chr) + 13
+      result += (lowercase[idx13])
+    else
+      result += chr
+    end
+  end
+
+  result
+end
+
+# additional solution added 07/03/2022
+UPPERCASE = ('A'..'Z').to_a * 2
+LOWERCASE = ('a'..'z').to_a * 2
+
+def decrypt(chr)
+  if UPPERCASE.include?(chr)
+    UPPERCASE[UPPERCASE.index(chr) + 13]
+  elsif LOWERCASE.include?(chr)
+    LOWERCASE[LOWERCASE.index(chr) + 13]
+  else
+    chr
+  end
+end
+
+def rot13(str)
+  str.chars.map { |chr| decrypt(chr) }.join
+end
+
+# additional solution added 07/03/2022
+LETTERS = ('A'..'Z').to_a * 2
+
+def decrypt(chr)
+  if LETTERS.include?(chr)
+    LETTERS[LETTERS.index(chr) + 13]
+  elsif LETTERS.include?(chr.upcase)
+    LETTERS[LETTERS.index(chr.upcase) + 13].downcase
+  else
+    chr
+  end
+end
+
+def rot13(str)
+  str.chars.map { |chr| decrypt(chr) }.join
+end
+
+
+p rot13('Nqn Ybirynpr')  == "Ada Lovelace"
+p rot13('Tenpr Ubccre') == "Grace Hopper"
+p rot13('Nqryr Tbyqfgvar') == "Adele Goldstine"
+p rot13('Nyna Ghevat') == "Alan Turing"
+p rot13('Puneyrf Onoontr') == "Charles Babbage"
+p rot13('Noqhyynu Zhunzznqova Zhfn ny-Xujnevmzv') == "Abdullah Muhammadbin Musa al-Khwarizmi"
+p rot13('Wbua Ngnanfbss') == "John Atanasoff"
+p rot13('Ybvf Unvog') == "Lois Haibt"
+p rot13('Pynhqr Funaaba')  == "Claude Shannon"
+p rot13('Fgrir Wbof') == "Steve Jobs"
+p rot13('Ovyy Tngrf') == "Bill Gates"
+p rot13('Gvz Orearef-Yrr') == "Tim Berners-Lee"
+p rot13('Fgrir Jbmavnx') == "Steve Wozniak"
+p rot13('Xbaenq Mhfr') == "Konrad Zuse"
+p rot13('Fve Nagbal Ubner') == "Sir Antony Hoare"
+p rot13('Zneiva Zvafxl') == "Marvin Minsky"
+p rot13('Lhxvuveb Zngfhzbg') == "Yukihiro Matsumot"
+p rot13('Unllvz Fybavzfxv') == "Hayyim Slonimski"
+p rot13('Tregehqr Oynapu') == "Gertrude Blanch"
 
 
 # method 2
