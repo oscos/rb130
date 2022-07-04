afdb8a98c84f13ebbb4ca5fe51c097d879317675
oscos <cortes.oscar@outlook.com>
2022-07-04 02:07:51 +0000

    Add new solution.

--- exercises/easy1_x10_count_items.rb
+++ exercises/easy1_x10_count_items.rb
@@ -8,6 +8,10 @@ def count(arr)
   counter
 end
 
+# new solution added 07/03/2022
+def count(arr)
+  arr.select {|n| yield(n) }.size
+end
 
 p count([1,2,3,4,5]) { |value| value.odd? } == 3
 p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
