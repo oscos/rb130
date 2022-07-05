abf33db072c4bd8e1d99e82ff1a294d1d8ce2d00
oscos <cortes.oscar@outlook.com>
2022-07-05 14:21:19 +0000

    Add new solution

--- exercises/easy2_x05_drop_while.rb
+++ exercises/easy2_x05_drop_while.rb
@@ -32,6 +32,14 @@ def drop_while(arr)
   flag ? arr[counter..-1] : []
 end
 
+# new solution added 07/04/2022
+def drop_while(arr)
+  arr.each_with_index do |n, idx|
+    return arr[idx..-1] unless yield(n) # if yield(n) == false
+  end
+  []
+end
+
 p drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6]
 p drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6]
 p drop_while([1, 3, 5, 6]) { |value| true } == []
